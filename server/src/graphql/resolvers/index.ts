9a0557952b247404a99b7636ce7a4deaaf28813b
Anshul Aggarwal <anshulaggarw4l@gmail.com>
2020-05-05 22:54:08 -0700

    removing the contact resolver that was there before to make way for the new

--- server/src/graphql/resolvers/index.ts
+++ server/src/graphql/resolvers/index.ts
@@ -1,7 +1,6 @@
 import { NonEmptyArray } from 'type-graphql'
 
 import UserResolver from './user'
-import ContactInfoResolver from './contactInfo'
 import CardVersionResolver from './cardVersion'
 
-export default [UserResolver, ContactInfoResolver, CardVersionResolver] as NonEmptyArray<Function>
+export default [UserResolver, CardVersionResolver] as NonEmptyArray<Function>
