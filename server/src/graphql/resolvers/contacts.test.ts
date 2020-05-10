76e3b80a10e1a928c48638c985b1a0d412dee1f4
Anshul Aggarwal <anshulaggarw4l@gmail.com>
2020-05-09 21:24:14 -0700

    fixing issues and adding extensive testing

--- server/src/graphql/resolvers/contacts.test.ts
+++ server/src/graphql/resolvers/contacts.test.ts
@@ -4,6 +4,7 @@ import { createMockUser } from 'src/__mocks__/models/User'
 import { createMockConnection } from 'src/__mocks__/models/Connection'
 import { PersonName } from 'src/models/subschemas'
 import { DocumentType } from '@typegoose/typegoose'
+import { Role } from 'src/models/User'
 
 beforeAll(async () => {
   await initDB()
@@ -19,7 +20,7 @@ describe('ContactsResolver', () => {
   })
 
   describe('contacts query', () => {
-    it("Happy Path - get's user's connections", async () => {
+    it("get's user's connections", async () => {
       const user_from = await createMockUser()
       const user_to = await createMockUser({
         name: { first: 'Jeff', middle: 'William', last: 'Winger' },
@@ -63,9 +64,123 @@ describe('ContactsResolver', () => {
 
       expect(response.data?.contacts).toMatchObject(expectedData)
     })
+
+    it('returns an empty list if no connections exists', async () => {
+      const user_from = await createMockUser()
+
+      const response = await execQuery({
+        source: `
+        query ContactsTestQuery {
+          contacts {
+            id
+            name {
+              first
+              middle
+              last
+            }
+            phoneNumber
+            email
+            notes
+            cardImageUrl
+            vcfUrl
+          }
+        }
+        `,
+        contextUser: user_from,
+      })
+
+      expect(response.data?.contacts?.length).toBe(0)
+    })
+
+    it("doesn't let non-admins query", async () => {
+      const user_from = await createMockUser()
+      const user_to = await createMockUser({
+        name: { first: 'Jeff', middle: 'William', last: 'Winger' },
+        email: 'fake_lawyer@greendale.com',
+        password: 'save-greendale',
+      })
+
+      const response = await execQuery({
+        source: `
+        query ContactsTestQuery($userId: String!) {
+          contacts(userId: $userId) {
+            id
+            name {
+              first
+              middle
+              last
+            }
+            phoneNumber
+            email
+            notes
+            cardImageUrl
+            vcfUrl
+          }
+        }
+        `,
+        variableValues: {
+          userId: user_to.id,
+        },
+        contextUser: user_from,
+      })
+      expect(response.errors[0].message).toContain('denied!')
+    })
+
+    it("lets admins query other user's connections", async () => {
+      const user_admin = await createMockUser({ roles: [Role.Admin] })
+      const user_from = await createMockUser({
+        name: { first: 'Annie', middle: 'Allison', last: 'Edison' },
+        email: 'goody2shoes@greendale.com',
+        password: 'save-greendale',
+      })
+      const user_to = await createMockUser({
+        name: { first: 'Jeff', middle: 'William', last: 'Winger' },
+        email: 'fake_lawyer@greendale.com',
+        password: 'save-greendale',
+      })
+      const connection = await createMockConnection({ from: user_from._id, to: user_to._id })
+
+      const response = await execQuery({
+        source: `
+        query ContactsTestQuery($userId: String!) {
+          contacts(userId: $userId) {
+            id
+            name {
+              first
+              middle
+              last
+            }
+            phoneNumber
+            email
+            notes
+            cardImageUrl
+            vcfUrl
+          }
+        }
+        `,
+        variableValues: {
+          userId: user_from.id,
+        },
+        contextUser: user_admin,
+      })
+
+      const expectedData = [
+        {
+          id: user_to.id,
+          name: (user_to.name as DocumentType<PersonName>).toObject(),
+          notes: connection.notes ?? null,
+          phoneNumber: user_to.phoneNumber ?? null,
+          email: user_to.email,
+          cardImageUrl: null,
+          vcfUrl: user_to.vcfUrl ?? null,
+        },
+      ]
+
+      expect(response.data?.contacts).toMatchObject(expectedData)
+    })
   })
   describe('contact query', () => {
-    it('Gets a user contact where connection exists and not admin', async () => {
+    it('gets a user contact where connection exists', async () => {
       const user_from = await createMockUser()
       const user_to = await createMockUser({
         name: { first: 'Jeff', middle: 'William', last: 'Winger' },
@@ -109,6 +224,40 @@ describe('ContactsResolver', () => {
       }
 
       expect(response.data?.contact).toMatchObject(expectedData)
+    })
+    it('fails if no connectin exists', async () => {
+      const user_from = await createMockUser()
+      const user_to = await createMockUser({
+        name: { first: 'Jeff', middle: 'William', last: 'Winger' },
+        email: 'fake_lawyer@greendale.com',
+        password: 'save-greendale',
+      })
+
+      const response = await execQuery({
+        source: `
+        query ContactTestQuery($contactId: String!) {
+          contact(contactId: $contactId) {
+            id
+            name {
+              first
+              middle
+              last
+            }
+            phoneNumber
+            email
+            notes
+            cardImageUrl
+            vcfUrl
+          }
+        }
+        `,
+        variableValues: {
+          contactId: user_to.id,
+        },
+        contextUser: user_from,
+      })
+
+      expect(response.errors).toBeDefined()
     })
   })
 })
--- server/src/graphql/resolvers/contacts.ts
+++ server/src/graphql/resolvers/contacts.ts
@@ -3,7 +3,7 @@ import MUUID from 'uuid-mongodb'
 
 import { AdminOnlyArgs } from '../auth'
 import { IApolloContext } from 'src/graphql/types'
-import User, { Role } from 'src/models/User'
+import User from 'src/models/User'
 import Connection from 'src/models/Connection'
 import { PersonName } from 'src/models/subschemas'
 import { UUIDScalar, UUIDType } from 'src/models/scalars'
@@ -11,6 +11,7 @@ import { CardVersion } from 'src/models/CardVersion'
 
 @ObjectType()
 class Contact {
+  //id of the user who's contact is being queried
   @Field((type) => UUIDScalar)
   id: UUIDType
 
@@ -23,6 +24,7 @@ class Contact {
   @Field({ nullable: true })
   email: string
 
+  //unique to the connections, notes taken by the user querying
   @Field({ nullable: true })
   notes: string
 
@@ -35,7 +37,10 @@ class Contact {
 
 @Resolver()
 class ContactsResolver {
-  @Authorized(Role.User)
+  // The `contacts` query is for a list of contacts that
+  // the querying user has as connections. This returns a list of all
+  // contacts the querying user has.
+  @Authorized()
   @AdminOnlyArgs('userId')
   @Query(() => [Contact])
   async contacts(
@@ -67,50 +72,34 @@ class ContactsResolver {
     })
   }
 
+  // The contact query returns a specific user's contact information
+  // It checks that the user is actually connected before returning the information
   @Query(() => Contact)
   async contact(
     @Arg('contactId', { nullable: false }) contactId: string,
     @Ctx() context: IApolloContext
   ): Promise<Contact> {
-    if (context.user.roles.includes(Role.Admin)) {
-      const requestedUser = await User.mongo
-        .findOne({ _id: MUUID.from(contactId) })
-        .populate({ path: 'defaultCardVersion' })
+    const connection = await Connection.mongo
+      .findOne({ from: context.user._id, to: MUUID.from(contactId) })
+      .populate({
+        path: 'to',
+        populate: {
+          path: 'defaultCardVersion',
+        },
+      })
 
-      return {
-        id: requestedUser._id,
-        name: requestedUser.name,
-        phoneNumber: requestedUser.phoneNumber,
-        email: requestedUser.email,
-        vcfUrl: requestedUser.vcfUrl,
-        cardImageUrl: (requestedUser.defaultCardVersion as CardVersion | null)?.imageUrl,
-        notes: null,
-      }
-    } else {
-      const connection = await Connection.mongo
-        .findOne({ from: context.user._id, to: MUUID.from(contactId) })
-        .populate({
-          path: 'from',
-          populate: {
-            path: 'defaultCardVersion',
-          },
-        })
-        .populate({
-          path: 'to',
-          populate: {
-            path: 'defaultCardVersion',
-          },
-        })
+    if (!connection) {
+      throw new Error('No connection exists between current user and queried user')
+    }
 
-      return {
-        id: (connection.to as User)._id,
-        name: (connection.to as User).name,
-        phoneNumber: (connection.to as User).phoneNumber,
-        email: (connection.to as User).email,
-        vcfUrl: (connection.to as User).vcfUrl,
-        cardImageUrl: ((connection.to as User).defaultCardVersion as CardVersion | null)?.imageUrl,
-        notes: connection.notes,
-      }
+    return {
+      id: (connection.to as User)._id,
+      name: (connection.to as User).name,
+      phoneNumber: (connection.to as User).phoneNumber,
+      email: (connection.to as User).email,
+      vcfUrl: (connection.to as User).vcfUrl,
+      cardImageUrl: ((connection.to as User).defaultCardVersion as CardVersion | null)?.imageUrl,
+      notes: connection.notes,
     }
   }
 }
