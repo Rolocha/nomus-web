0cc94e320feb273c8d4823f1c40a363d8aa38988
Bibek <ghim.bibek@gmail.com>
2020-05-22 21:05:51 -0700

    Make UCP menu mobile-friendly

--- client/src/styles/theme.ts
+++ client/src/styles/theme.ts
@@ -4,6 +4,7 @@ import breakpoints from './breakpoints'
 
 const theme = {
   space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
+  radii: [0, 4, 8, 16, 32],
   fontSizes: [18, 20, 24, 54],
   fontFamilies: typography.fontFamilies,
   textStyles: typography.textStyles,
