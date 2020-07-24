63c3476812cb2df80124fd88949be2c4c1ecc307
Bibek <ghim.bibek@gmail.com>
2020-07-23 22:55:01 -0700

    Pull dashboard layout into a MultiWorkspace comp

---
+++ client/src/components/MultiWorkspace.tsx
@@ -0,0 +1,153 @@
+import { css } from '@emotion/core'
+import * as React from 'react'
+import * as Text from 'src/components/Text'
+import Box from 'src/components/Box'
+import {
+  Redirect,
+  Route,
+  Switch,
+  useLocation,
+  useRouteMatch,
+} from 'react-router-dom'
+import { colors } from 'src/styles'
+import { InternalLink } from 'src/components/Link'
+import breakpoints, { mq } from 'src/styles/breakpoints'
+
+interface TabItem {
+  Icon: (...args: any) => JSX.Element
+  label: React.ReactNode
+  content: React.ReactNode
+  path?: string
+  linkPath?: string
+  matchPath?: string
+}
+
+interface Props {
+  children: Array<TabItem>
+}
+
+const bp = 'md'
+const POINTY_TAB_INDICATOR = css`
+  ${mq[bp]} {
+    &:after {
+      content: ' ';
+      display: block;
+      width: 1rem;
+      height: 1rem;
+      position: absolute;
+      top: 50%;
+      left: 100%;
+      transform: translate(-50%, -50%) rotate(45deg);
+      background-color: ${colors.nomusBlue};
+    }
+  }
+`
+
+const MultiWorkspace = ({ children: tabs }: Props) => {
+  const routeMatch = useRouteMatch()
+  const location = useLocation()
+
+  return (
+    <Box
+      display="flex"
+      flexDirection={{ _: 'column', [bp]: 'row' }}
+      alignItems={{ [bp]: 'flex-start' }}
+    >
+      {/* Menu for selecting dashboard section */}
+      <Box
+        display="flex"
+        flexDirection={{ _: 'row', [bp]: 'column' }}
+        flexShrink={0}
+        minWidth={{ [bp]: 200 }}
+        bg={colors.twilight}
+        borderTopLeftRadius={{ _: 0, [bp]: 3 }}
+        borderBottomLeftRadius={{ _: 0, [bp]: 3 }}
+        // Needed to ensure the current-tab caret indicator is visible
+        overflow="visible"
+      >
+        {tabs.map(({ linkPath, path, Icon, label }, index) => {
+          const sectionPath = `${routeMatch.url}/${linkPath ?? path}`
+          const isCurrentSection = location.pathname.startsWith(sectionPath)
+          return (
+            <Box
+              key={path}
+              borderTopLeftRadius={{ _: 0, [bp]: index === 0 ? 3 : 0 }}
+              borderBottomLeftRadius={{
+                _: 0,
+                [bp]: index === tabs.length - 1 ? 3 : 0,
+              }}
+              bg={isCurrentSection ? colors.nomusBlue : undefined}
+              flexBasis={{
+                _: `${100 / tabs.length}%`,
+                [bp]: 'auto',
+              }}
+              position="relative"
+              css={isCurrentSection ? POINTY_TAB_INDICATOR : null}
+            >
+              <InternalLink to={sectionPath}>
+                <Box
+                  py="24px"
+                  px={3}
+                  display="flex"
+                  flexDirection={{ _: 'column', [bp]: 'row' }}
+                  alignItems="center"
+                >
+                  <Icon
+                    color="white"
+                    css={css`
+                      height: 1.5em;
+                      // Margin below in mobile; on right in desktop
+                      margin-bottom: 0.5em;
+                      ${mq[bp]} {
+                        margin-bottom: 0;
+                        margin-right: 0.7em;
+                      }
+                    `}
+                  />
+                  <Text.Plain
+                    m={0}
+                    color="white"
+                    fontSize={{ _: 10, [bp]: 'unset' }}
+                    fontWeight={isCurrentSection ? 500 : 'undefined'}
+                  >
+                    {label}
+                  </Text.Plain>
+                </Box>
+              </InternalLink>
+            </Box>
+          )
+        })}
+      </Box>
+
+      {/* Content for selected section */}
+      <Box
+        flexGrow={1}
+        boxShadow={{ [bp]: '0px 0px 4px rgba(0, 0, 0, 0.25)' }}
+        bg="white"
+        p={{ _: '24px', [bp]: '48px' }}
+        // py={{ _: 4, [bp]: 5 }}
+        borderTopRightRadius={{ [bp]: 3 }}
+        borderBottomRightRadius={{ [bp]: 3 }}
+        borderBottomLeftRadius={{ [bp]: 3 }}
+        // Just for now
+        minHeight="50vh"
+      >
+        <Switch>
+          {tabs.map(({ matchPath, path, content }) => (
+            <Route key={path} path={`${routeMatch.url}/${matchPath ?? path}`}>
+              {content}
+            </Route>
+          ))}
+          {/* If user lands on a route that doesn't match any, redirect to the first one */}
+          <Route>
+            <Redirect
+              to={`${routeMatch.url}/${tabs[0].linkPath ?? tabs[0].path}`}
+            />
+          </Route>
+        </Switch>
+      </Box>
+    </Box>
+  )
+}
+
+export default MultiWorkspace
--- client/src/pages/UserControlPanel/index.tsx
+++ client/src/pages/UserControlPanel/index.tsx
@@ -1,16 +1,9 @@
-import { css } from '@emotion/core'
 import * as React from 'react'
-import {
-  Redirect,
-  Route,
-  Switch,
-  useLocation,
-  useRouteMatch,
-} from 'react-router-dom'
+import { useLocation, useRouteMatch } from 'react-router-dom'
 import { gql, useQuery } from 'src/apollo'
 import { UserControlPanelSkeletonQuery } from 'src/apollo/types/UserControlPanelSkeletonQuery'
 import Box from 'src/components/Box'
-import { InternalLink } from 'src/components/Link'
+import MultiWorkspace from 'src/components/MultiWorkspace'
 import Navbar from 'src/components/Navbar'
 import * as SVG from 'src/components/SVG'
 import * as Text from 'src/components/Text'
@@ -25,21 +18,6 @@ import ProfileSection from './ProfileSection'
 import SettingsSection from './SettingsSection'
 
 const bp = 'md'
-const POINTY_TAB_INDICATOR = css`
-  ${mq[bp]} {
-    &:after {
-      content: ' ';
-      display: block;
-      width: 1rem;
-      height: 1rem;
-      position: absolute;
-      top: 50%;
-      left: 100%;
-      transform: translate(-50%, -50%) rotate(45deg);
-      background-color: ${theme.colors.nomusBlue};
-    }
-  }
-`
 
 const ProfilePage = () => {
   const { loading, data } = useQuery<UserControlPanelSkeletonQuery>(
@@ -61,41 +39,6 @@ const ProfilePage = () => {
   if (loading || !data) {
     return <LoadingPage />
   }
-
-  const controlPanelSections = [
-    {
-      path: 'profile',
-      label: 'Profile',
-      Icon: SVG.Profile,
-      Component: ProfileSection,
-    },
-    {
-      path: 'cards',
-      label: 'Cards',
-      Icon: SVG.Cards,
-      Component: CardsSection,
-    },
-    {
-      linkPath: 'contacts',
-      matchPath: 'contacts/:viewMode?/:usernameOrId?',
-      label: 'Contacts',
-      Icon: SVG.Contacts,
-      Component: ContactsSection,
-    },
-    {
-      linkPath: 'orders',
-      matchPath: 'orders/:orderId?',
-      label: 'Orders',
-      Icon: SVG.Orders,
-      Component: OrdersSection,
-    },
-    {
-      path: 'settings',
-      label: 'Settings',
-      Icon: SVG.Settings,
-      Component: SettingsSection,
-    },
-  ]
 
   return (
     <Box
@@ -127,116 +70,42 @@ const ProfilePage = () => {
             </Text.PageHeader>
           </Box>
         )}
-        <Box
-          display="flex"
-          flexDirection={{ _: 'column', [bp]: 'row' }}
-          alignItems={{ [bp]: 'flex-start' }}
-        >
-          {/* Menu for selecting dashboard section */}
-          <Box
-            display="flex"
-            flexDirection={{ _: 'row', [bp]: 'column' }}
-            flexShrink={0}
-            minWidth={{ [bp]: 200 }}
-            bg={theme.colors.twilight}
-            borderTopLeftRadius={{ _: 0, [bp]: 3 }}
-            borderBottomLeftRadius={{ _: 0, [bp]: 3 }}
-            // Needed to ensure the current-tab caret indicator is visible
-            overflow="visible"
-          >
-            {controlPanelSections.map(
-              ({ linkPath, path, Icon, label }, index) => {
-                const sectionPath = `${routeMatch.url}/${linkPath ?? path}`
-                const isCurrentSection = location.pathname.startsWith(
-                  sectionPath,
-                )
-                return (
-                  <Box
-                    key={path}
-                    borderTopLeftRadius={{ _: 0, [bp]: index === 0 ? 3 : 0 }}
-                    borderBottomLeftRadius={{
-                      _: 0,
-                      [bp]: index === controlPanelSections.length - 1 ? 3 : 0,
-                    }}
-                    bg={isCurrentSection ? theme.colors.nomusBlue : undefined}
-                    flexBasis={{
-                      _: `${100 / controlPanelSections.length}%`,
-                      [bp]: 'auto',
-                    }}
-                    position="relative"
-                    css={isCurrentSection ? POINTY_TAB_INDICATOR : null}
-                  >
-                    <InternalLink to={sectionPath}>
-                      <Box
-                        py="24px"
-                        px={3}
-                        display="flex"
-                        flexDirection={{ _: 'column', [bp]: 'row' }}
-                        alignItems="center"
-                      >
-                        <Icon
-                          color="white"
-                          css={css`
-                            height: 1.5em;
-
-                            // Margin below in mobile; on right in desktop
-                            margin-bottom: 0.5em;
-                            ${mq[bp]} {
-                              margin-bottom: 0;
-                              margin-right: 0.7em;
-                            }
-                          `}
-                        />
-                        <Text.Plain
-                          m={0}
-                          color="white"
-                          fontSize={{ _: 10, [bp]: 'unset' }}
-                          fontWeight={isCurrentSection ? 500 : 'undefined'}
-                        >
-                          {label}
-                        </Text.Plain>
-                      </Box>
-                    </InternalLink>
-                  </Box>
-                )
-              },
-            )}
-          </Box>
-
-          {/* Content for selected section */}
-          <Box
-            flexGrow={1}
-            boxShadow={{ [bp]: '0px 0px 4px rgba(0, 0, 0, 0.25)' }}
-            bg="white"
-            p={{ _: '24px', [bp]: '48px' }}
-            // py={{ _: 4, [bp]: 5 }}
-            borderTopRightRadius={{ [bp]: 3 }}
-            borderBottomRightRadius={{ [bp]: 3 }}
-            borderBottomLeftRadius={{ [bp]: 3 }}
-            // Just for now
-            minHeight="50vh"
-          >
-            <Switch>
-              {controlPanelSections.map(({ matchPath, path, Component }) => (
-                <Route
-                  key={path}
-                  path={`${routeMatch.url}/${matchPath ?? path}`}
-                >
-                  <Component />
-                </Route>
-              ))}
-              {/* If user lands on a route that doesn't match any, redirect to the first one */}
-              <Route>
-                <Redirect
-                  to={`${routeMatch.url}/${
-                    controlPanelSections[0].linkPath ??
-                    controlPanelSections[0].path
-                  }`}
-                />
-              </Route>
-            </Switch>
-          </Box>
-        </Box>
+        <MultiWorkspace
+          children={[
+            {
+              path: 'profile',
+              label: 'Profile',
+              Icon: SVG.Profile,
+              content: <ProfileSection />,
+            },
+            {
+              path: 'cards',
+              label: 'Cards',
+              Icon: SVG.Cards,
+              content: <CardsSection />,
+            },
+            {
+              linkPath: 'contacts',
+              matchPath: 'contacts/:viewMode?/:usernameOrId?',
+              label: 'Contacts',
+              Icon: SVG.Contacts,
+              content: <ContactsSection />,
+            },
+            {
+              linkPath: 'orders',
+              matchPath: 'orders/:orderId?',
+              label: 'Orders',
+              Icon: SVG.Orders,
+              content: <OrdersSection />,
+            },
+            {
+              path: 'settings',
+              label: 'Settings',
+              Icon: SVG.Settings,
+              content: <SettingsSection />,
+            },
+          ]}
+        />
       </Box>
     </Box>
   )
