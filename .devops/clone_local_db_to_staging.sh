3e8ff7092d6ad2831b764f7a5219d588ce6f66f6
Bibek <ghim.bibek@gmail.com>
2020-10-11 18:14:15 -0700

    Update clone_local_to_staging script

--- .devops/clone_local_db_to_staging.sh
+++ .devops/clone_local_db_to_staging.sh
@@ -3,8 +3,8 @@
 # Run this via docker exec -i db bash < ./clone_local_db_to_staging.sh
 
 echo 'grabbing local db'
-mongodump -h localhost:27017 -d rolocha-dev -u nomus -p admin -o /tmp/nomus-db-backup
+mongodump -h localhost:27017 -d nomus-dev -u nomus -p nomus -o /tmp/nomus-db-backup
 echo 'clearing out staging db'
-mongo ec2-52-20-46-100.compute-1.amazonaws.com/rolocha-staging -u nomus -p jZHfJdBK2Bo --eval "printjson(db.dropDatabase())"
+mongo ec2-52-20-46-100.compute-1.amazonaws.com/nomus-dev -u nomus -p jZHfJdBK2Bo --eval "printjson(db.dropDatabase())"
 echo 'transferring local db backup to staging db'
-mongorestore -h ec2-52-20-46-100.compute-1.amazonaws.com -d rolocha-staging -u nomus -p jZHfJdBK2Bo /tmp/nomus-db-backup/rolocha-dev
+mongorestore -h ec2-52-20-46-100.compute-1.amazonaws.com -d nomus-dev -u nomus -p jZHfJdBK2Bo /tmp/nomus-db-backup/nomus-dev
