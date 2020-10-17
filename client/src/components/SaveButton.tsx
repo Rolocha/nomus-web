dbb4c76bc83bf3d82c320c0bebdc31076e4b2f45
Anshul Aggarwal <anshulaggarw4l@gmail.com>
2020-10-16 23:20:42 -0700

    changing the save button to have text on the right

--- client/src/components/SaveButton.tsx
+++ client/src/components/SaveButton.tsx
@@ -14,13 +14,6 @@ type SaveButtonProps = {
 const SaveButton = ({ iconOnlyBp, onClick }: SaveButtonProps) => (
   <Button onClick={onClick} variant="success">
     <Box display="flex" flexDirection="row" alignItems="center">
-      <Box
-        display={iconOnlyBp ? { _: 'none', [iconOnlyBp]: 'block' } : 'block'}
-      >
-        <Text.Body2 fontSize="14px" color="validGreen" ml={1}>
-          Save
-        </Text.Body2>
-      </Box>
       <SVG.Check
         css={css`
           width: 20px;
@@ -28,6 +21,13 @@ const SaveButton = ({ iconOnlyBp, onClick }: SaveButtonProps) => (
         `}
         color={colors.validGreen}
       />
+      <Box
+        display={iconOnlyBp ? { _: 'none', [iconOnlyBp]: 'block' } : 'block'}
+      >
+        <Text.Body2 fontSize="14px" color="validGreen" ml={1}>
+          Save
+        </Text.Body2>
+      </Box>
     </Box>
   </Button>
 )
