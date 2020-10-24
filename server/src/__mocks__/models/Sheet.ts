32fdf9cfb0adbf51b0e54085d502db296d5f0e17
Anshul Aggarwal <anshulaggarw4l@gmail.com>
2020-10-24 00:57:54 -0700

    removing UserRoute bc no longer necessary

--- server/src/__mocks__/models/Sheet.ts
+++ server/src/__mocks__/models/Sheet.ts
@@ -4,11 +4,7 @@ import { createMockUser } from './User'
 
 export const createMockSheet = async (sheetOverride: Partial<Sheet> = {}) => {
   const mockUser = await createMockUser()
-  // let cardArray: Card[] = []
-  // for (let i = 0; i < 25; i++) {
-  //   const card = await createMockCard({ user: mockUser._id })
-  //   cardArray.push(card)
-  // }
+
   const newSheetPayload: Partial<Sheet> = {
     cardVersion: await createMockCardVersion({ user: mockUser._id }),
     cards: [],
--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,7 +1,6 @@
 export { default as User } from './User'
 export { default as Card } from './Card'
 export { default as CardVersion } from './CardVersion'
-export { default as UserRoute } from './UserRoute'
 export { default as Interaction } from './Interaction'
 export { default as Token } from './Token'
 export { default as Connection } from './Connection'
