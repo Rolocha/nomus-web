79ca3b18c32471ac725418d03c794676f24b3b3b
Anshul Aggarwal <anshulaggarw4l@gmail.com>
2020-11-21 00:24:20 -0800

    first steps for linker

--- client/src/pages/Linker.tsx
+++ client/src/pages/Linker.tsx
@@ -1,8 +1,66 @@
 import * as React from 'react'
 import Box from 'src/components/Box'
+import Navbar from 'src/components/Navbar'
+import * as Text from 'src/components/Text'
+
+const bp = 'md'
 
 const Linker = () => {
-  return <Box>Linker Page!</Box>
+  return (
+    <Box>
+      <Navbar />
+      <Box
+        container
+        display="flex"
+        flexDirection="column"
+        alignItems="stretch"
+        justifyContent="flex-start"
+        bg="white"
+        position="relative"
+      >
+        <Box
+          display="grid"
+          gridTemplateAreas={{
+            _: `
+              "heading"
+              "explain"
+              "instructions"
+              "inputForm"
+              "send"
+            `,
+            [bp]: `
+              "heading"
+              "explain"
+              "instructions"
+              "inputForm"
+              "send"
+            `,
+          }}
+          gridColumnGap={3}
+          gridRowGap={3}
+        >
+          <Box gridArea="heading" placeSelf="center">
+            <Text.PageHeader>Sheet Linker</Text.PageHeader>
+          </Box>
+
+          <Box gridArea="explain">
+            <Text.Body>This page is to link cards to a User.</Text.Body>
+            <Text.Body>
+              For any questions, email help@nomus.me and we'll get it sorted!
+            </Text.Body>
+          </Box>
+          <Box gridArea="instructions">
+            <Text.Body>
+              Please enter the 6 digit alphanumeric ID found on the long edge of
+              the printed sheet below
+            </Text.Body>
+          </Box>
+          <Box gridArea="inputForm">Hi</Box>
+          <Box gridArea="send">Button</Box>
+        </Box>
+      </Box>
+    </Box>
+  )
 }
 
 export default Linker
