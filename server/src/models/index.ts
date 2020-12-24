24a3a663b8dbf5756ad74cf093c92cc534b1f78a
Bibek <ghim.bibek@gmail.com>
2020-12-24 13:07:41 -0800

    Replace Interaction model with CardInteraction

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,7 +1,7 @@
 export { default as User } from './User'
 export { default as Card } from './Card'
 export { default as CardVersion } from './CardVersion'
-export { default as Interaction } from './Interaction'
+export { default as CardInteraction } from './CardInteraction'
 export { default as Token } from './Token'
 export { default as Connection } from './Connection'
 export { default as Order } from './Order'
