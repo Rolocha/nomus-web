bc86b5d8f37b9e304059f4bfcfb1e19ef62345b0
Bibek <ghim.bibek@gmail.com>
2021-01-06 08:28:46 -0800

    Move /api routes around a little to separate code

---
+++ server/src/api/contact.ts
@@ -0,0 +1,52 @@
+import * as fs from 'fs'
+import express from 'express'
+import { User } from 'src/models'
+import { saveVCFToFile } from 'src/util/vcf'
+import vCards from 'vcards-js'
+
+export const contactRouter = express.Router()
+
+contactRouter.get('/vcf/:username', async (req: express.Request, res: express.Response) => {
+  const username = req.params.username
+  const meetingDate = req.query.meetingDate ? new Date(req.query.meetingDate as string) : null
+  const meetingPlace = req.query.meetingPlace as string
+  const tags = req.query.tags as string
+  const notes = req.query.notes as string
+
+  if (username == null) {
+    return res.status(400).end()
+  }
+
+  const contact = await User.mongo.findOne({ username })
+  if (contact == null) {
+    return res.status(400).end()
+  }
+
+  const contactPhotoUrl = await contact.getProfilePicDataUrl()
+  const vCard = vCards()
+  vCard.firstName = contact.name.first
+  vCard.middleName = contact.name.middle
+  vCard.lastName = contact.name.last
+  vCard.cellPhone = contact.phoneNumber
+  vCard.photo.embedFromString(contactPhotoUrl, 'image/png')
+  vCard.note = [
+    meetingDate != null ? `Meeting Date: ${meetingDate.toDateString()}` : null,
+    meetingPlace != null ? `Meeting Place: ${meetingPlace}` : null,
+    tags ? `Tags: ${tags}` : null,
+    notes ? `Additional notes:\n${notes}` : null,
+  ]
+    .filter(Boolean)
+    .join('\n')
+
+  const fileName = `${contact.name.first}-${contact.name.last}.vcf`
+  const filePath = saveVCFToFile(vCard, fileName)
+
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      console.log('Failure')
+      console.log(err)
+    } else {
+      fs.unlinkSync(filePath)
+    }
+  })
+})
