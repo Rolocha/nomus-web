273c910cec8584aaf12d2747feeed27db8a25bf2
Anshul Aggarwal <anshulaggarw4l@gmail.com>
2021-01-08 11:05:32 -0800

    reformating tests

--- server/src/graphql/resolvers/order.test.ts
+++ server/src/graphql/resolvers/order.test.ts
@@ -22,235 +22,18 @@ describe('OrderResolver', () => {
   })
 
   describe('order', () => {
-    it("skipping tests for now since they don't pass", () => {})
-    //   it('fetches a single order for admin user', async () => {
-    //     const order = await createMockOrder()
-
-    //     const response = await execQuery({
-    //       source: `
-    //       query OrderTestQuery($orderId: String) {
-    //         order(orderId: $orderId) {
-    //           id
-    //         }
-    //       }
-    //       `,
-    //       variableValues: {
-    //         orderId: order.id,
-    //       },
-    //       asAdmin: true,
-    //     })
-
-    //     expect(response.data?.order?.id).toBe(order.id)
-    //   })
-    // })
-
-    // describe('orders', () => {
-    //   it('fetches all orders for a user', async () => {
-    //     const user = await createMockUser()
-    //     const order1 = await createMockOrder({ user: user })
-    //     const order2 = await createMockOrder({ user: user })
-
-    //     const response = await execQuery({
-    //       source: `
-    //       query OrdersTestQuery {
-    //         orders {
-    //           id
-    //         }
-    //       }
-    //       `,
-    //       contextUser: user,
-    //     })
-
-    //     expect(response.data?.orders).toEqual([{ id: order1.id }, { id: order2.id }])
-    //   })
-    //   it('fetches all users orders for an admin', async () => {
-    //     const user = await createMockUser()
-    //     const order1 = await createMockOrder({ user: user })
-    //     const order2 = await createMockOrder({ user: user })
-
-    //     const response = await execQuery({
-    //       source: `
-    //       query OrdersTestQuery($userId: String!) {
-    //         orders(userId: $userId) {
-    //           id
-    //         }
-    //       }
-    //       `,
-    //       variableValues: {
-    //         userId: user.id,
-    //       },
-    //       asAdmin: true,
-    //     })
-
-    //     expect(response.data?.orders).toEqual([{ id: order1.id }, { id: order2.id }])
-    //   })
-    // })
-
-    // describe('createNewOrder', () => {
-    //   it('creates a new order for a user', async () => {
-    //     const user = await createMockUser()
-
-    //     const orderPayload = {
-    //       quantity: 25,
-    //       price: 29.99,
-    //     }
-    //     const cardVersionPayload = {
-    //       cardSlug: 'slug',
-    //       firstName: 'A',
-    //       middleName: 'B',
-    //       lastName: 'C',
-    //       phoneNumber: '1234567890',
-    //       email: 'newone@gmail.com',
-    //       title: 'president',
-    //       company: 'united states',
-    //       addressLine1: '637 Levering',
-    //       addressLine2: '123',
-    //       addressLine3: 'Los Angeles, CA, 90072',
-    //       frontImageUrl: 's3://some_url_front',
-    //       backImageUrl: 's3://some_url_back',
-    //     }
-
-    //     const response = await execQuery({
-    //       source: `
-    //         mutation CreateNewOrderTestQuery($userId: String, $orderPayload: OrderGenerationInput!, $cardVersionPayload: CardVersionInput!) {
-    //           createNewOrder(userId: $userId, orderPayload: $orderPayload, cardVersionPayload: $cardVersionPayload) {
-    //             orderId
-    //             cardVersionId
-    //           }
-    //         }
-    //       `,
-    //       variableValues: {
-    //         orderPayload: orderPayload,
-    //         cardVersionPayload: cardVersionPayload,
-    //       },
-    //       contextUser: user,
-    //     })
-
-    //     const newOrder = await Order.mongo.findById(response.data?.createNewOrder?.orderId)
-    //     const newCardVersion = await CardVersion.mongo.findById(
-    //       response.data?.createNewOrder?.cardVersionId
-    //     )
-
-    //     expect(newOrder.quantity).toEqual(orderPayload.quantity)
-    //     expect(newOrder.price).toEqual(orderPayload.price)
-    //     expect(newOrder.state).toEqual(OrderState.Captured)
-
-    //     const partialNewCardVersion = {
-    //       cardSlug: newCardVersion.cardSlug,
-    //       firstName: newCardVersion.name.first,
-    //       middleName: newCardVersion.name.middle,
-    //       lastName: newCardVersion.name.last,
-    //       phoneNumber: newCardVersion.phoneNumber,
-    //       email: newCardVersion.email,
-    //       title: newCardVersion.title,
-    //       company: newCardVersion.company,
-    //       addressLine1: newCardVersion.address.line1,
-    //       addressLine2: newCardVersion.address.line2,
-    //       addressLine3: newCardVersion.address.line3,
-    //       frontImageUrl: newCardVersion.frontImageUrl,
-    //       backImageUrl: newCardVersion.backImageUrl,
-    //     }
-    //     expect(partialNewCardVersion).toMatchObject(cardVersionPayload)
-    //   })
-    //   it('creates a new order for a user as an admin', async () => {
-    //     const user = await createMockUser()
-
-    //     const orderPayload = {
-    //       quantity: 25,
-    //       price: 29.99,
-    //     }
-    //     const cardVersionPayload = {
-    //       cardSlug: 'slug',
-    //       firstName: 'A',
-    //       middleName: 'B',
-    //       lastName: 'C',
-    //       phoneNumber: '1234567890',
-    //       email: 'newone@gmail.com',
-    //       title: 'president',
-    //       company: 'united states',
-    //       addressLine1: '637 Levering',
-    //       addressLine2: '123',
-    //       addressLine3: 'Los Angeles, CA, 90072',
-    //       frontImageUrl: 's3://some_url_front',
-    //       backImageUrl: 's3://some_url_back',
-    //     }
-
-    //     const response = await execQuery({
-    //       source: `
-    //         mutation CreateNewOrderTestQuery($userId: String, $orderPayload: OrderGenerationInput!, $cardVersionPayload: CardVersionInput!) {
-    //           createNewOrder(userId: $userId, orderPayload: $orderPayload, cardVersionPayload: $cardVersionPayload) {
-    //             orderId
-    //             cardVersionId
-    //           }
-    //         }
-    //       `,
-    //       variableValues: {
-    //         userId: user.id,
-    //         orderPayload: orderPayload,
-    //         cardVersionPayload: cardVersionPayload,
-    //       },
-    //       asAdmin: true,
-    //     })
-
-    //     const newOrder = await Order.mongo.findById(response.data?.createNewOrder?.orderId)
-    //     const newCardVersion = await CardVersion.mongo.findById(
-    //       response.data?.createNewOrder?.cardVersionId
-    //     )
-
-    //     expect(newOrder.quantity).toEqual(orderPayload.quantity)
-    //     expect(newOrder.price).toEqual(orderPayload.price)
-    //     expect(newOrder.state).toEqual(OrderState.Captured)
-
-    //     const partialNewCardVersion = {
-    //       cardSlug: newCardVersion.cardSlug,
-    //       firstName: newCardVersion.name.first,
-    //       middleName: newCardVersion.name.middle,
-    //       lastName: newCardVersion.name.last,
-    //       phoneNumber: newCardVersion.phoneNumber,
-    //       email: newCardVersion.email,
-    //       title: newCardVersion.title,
-    //       company: newCardVersion.company,
-    //       addressLine1: newCardVersion.address.line1,
-    //       addressLine2: newCardVersion.address.line2,
-    //       addressLine3: newCardVersion.address.line3,
-    //       frontImageUrl: newCardVersion.frontImageUrl,
-    //       backImageUrl: newCardVersion.backImageUrl,
-    //     }
-    //     expect(partialNewCardVersion).toMatchObject(cardVersionPayload)
-
-    //     const orderUser = await User.mongo.findById(newOrder.user)
-    //     const cardVersionUser = await User.mongo.findById(newCardVersion.user)
-
-    //     expect(orderUser.id).toBe(user.id)
-    //     expect(cardVersionUser.id).toBe(user.id)
-    //   })
-  })
-})
-
-describe('OrderModel', () => {
-  afterEach(async () => {
-    await dropAllCollections()
-  })
-
-  describe('order', () => {
-    it('detects a shortId collision and retries', async () => {
-      const order1 = await createMockOrder({ shortId: 'SJC123' })
-      const order2 = await createMockOrder({ shortId: 'SJC123' })
-      expect(order1.shortId).toBe('SJC123')
-      expect(order1.shortId).not.toEqual(order2.shortId)
-    })
     it('fetches a single order for non-admin user', async () => {
       const user = await createMockUser()
       const order = await createMockOrder({ user: user })
 
       const response = await execQuery({
         source: `
-          query OrderTestQuery($orderId: String) {
-            order(orderId: $orderId) {
-              id
-            }
+        query OrderTestQuery($orderId: String) {
+          order(orderId: $orderId) {
+            id
           }
-          `,
+        }
+        `,
         variableValues: {
           orderId: order.id,
         },
@@ -283,6 +66,215 @@ describe('OrderModel', () => {
       })
 
       expect(response.errors[0]?.message).toBe('User is not authorized to access order')
+    })
+    it('fetches a single order for admin user', async () => {
+      const order = await createMockOrder()
+
+      const response = await execQuery({
+        source: `
+          query OrderTestQuery($orderId: String) {
+            order(orderId: $orderId) {
+              id
+            }
+          }
+          `,
+        variableValues: {
+          orderId: order.id,
+        },
+        asAdmin: true,
+      })
+
+      expect(response.data?.order?.id).toBe(order.id)
+    })
+  })
+
+  // describe('orders', () => {
+  //   it('fetches all orders for a user', async () => {
+  //     const user = await createMockUser()
+  //     const order1 = await createMockOrder({ user: user })
+  //     const order2 = await createMockOrder({ user: user })
+
+  //     const response = await execQuery({
+  //       source: `
+  //       query OrdersTestQuery {
+  //         orders {
+  //           id
+  //         }
+  //       }
+  //       `,
+  //       contextUser: user,
+  //     })
+
+  //     expect(response.data?.orders).toEqual([{ id: order1.id }, { id: order2.id }])
+  //   })
+  //   it('fetches all users orders for an admin', async () => {
+  //     const user = await createMockUser()
+  //     const order1 = await createMockOrder({ user: user })
+  //     const order2 = await createMockOrder({ user: user })
+
+  //     const response = await execQuery({
+  //       source: `
+  //       query OrdersTestQuery($userId: String!) {
+  //         orders(userId: $userId) {
+  //           id
+  //         }
+  //       }
+  //       `,
+  //       variableValues: {
+  //         userId: user.id,
+  //       },
+  //       asAdmin: true,
+  //     })
+
+  //     expect(response.data?.orders).toEqual([{ id: order1.id }, { id: order2.id }])
+  //   })
+  // })
+
+  // describe('createNewOrder', () => {
+  //   it('creates a new order for a user', async () => {
+  //     const user = await createMockUser()
+
+  //     const orderPayload = {
+  //       quantity: 25,
+  //       price: 29.99,
+  //     }
+  //     const cardVersionPayload = {
+  //       cardSlug: 'slug',
+  //       firstName: 'A',
+  //       middleName: 'B',
+  //       lastName: 'C',
+  //       phoneNumber: '1234567890',
+  //       email: 'newone@gmail.com',
+  //       title: 'president',
+  //       company: 'united states',
+  //       addressLine1: '637 Levering',
+  //       addressLine2: '123',
+  //       addressLine3: 'Los Angeles, CA, 90072',
+  //       frontImageUrl: 's3://some_url_front',
+  //       backImageUrl: 's3://some_url_back',
+  //     }
+
+  //     const response = await execQuery({
+  //       source: `
+  //         mutation CreateNewOrderTestQuery($userId: String, $orderPayload: OrderGenerationInput!, $cardVersionPayload: CardVersionInput!) {
+  //           createNewOrder(userId: $userId, orderPayload: $orderPayload, cardVersionPayload: $cardVersionPayload) {
+  //             orderId
+  //             cardVersionId
+  //           }
+  //         }
+  //       `,
+  //       variableValues: {
+  //         orderPayload: orderPayload,
+  //         cardVersionPayload: cardVersionPayload,
+  //       },
+  //       contextUser: user,
+  //     })
+
+  //     const newOrder = await Order.mongo.findById(response.data?.createNewOrder?.orderId)
+  //     const newCardVersion = await CardVersion.mongo.findById(
+  //       response.data?.createNewOrder?.cardVersionId
+  //     )
+
+  //     expect(newOrder.quantity).toEqual(orderPayload.quantity)
+  //     expect(newOrder.price).toEqual(orderPayload.price)
+  //     expect(newOrder.state).toEqual(OrderState.Captured)
+
+  //     const partialNewCardVersion = {
+  //       cardSlug: newCardVersion.cardSlug,
+  //       firstName: newCardVersion.name.first,
+  //       middleName: newCardVersion.name.middle,
+  //       lastName: newCardVersion.name.last,
+  //       phoneNumber: newCardVersion.phoneNumber,
+  //       email: newCardVersion.email,
+  //       title: newCardVersion.title,
+  //       company: newCardVersion.company,
+  //       addressLine1: newCardVersion.address.line1,
+  //       addressLine2: newCardVersion.address.line2,
+  //       addressLine3: newCardVersion.address.line3,
+  //       frontImageUrl: newCardVersion.frontImageUrl,
+  //       backImageUrl: newCardVersion.backImageUrl,
+  //     }
+  //     expect(partialNewCardVersion).toMatchObject(cardVersionPayload)
+  //   })
+  //   it('creates a new order for a user as an admin', async () => {
+  //     const user = await createMockUser()
+
+  //     const orderPayload = {
+  //       quantity: 25,
+  //       price: 29.99,
+  //     }
+  //     const cardVersionPayload = {
+  //       cardSlug: 'slug',
+  //       firstName: 'A',
+  //       middleName: 'B',
+  //       lastName: 'C',
+  //       phoneNumber: '1234567890',
+  //       email: 'newone@gmail.com',
+  //       title: 'president',
+  //       company: 'united states',
+  //       addressLine1: '637 Levering',
+  //       addressLine2: '123',
+  //       addressLine3: 'Los Angeles, CA, 90072',
+  //       frontImageUrl: 's3://some_url_front',
+  //       backImageUrl: 's3://some_url_back',
+  //     }
+
+  //     const response = await execQuery({
+  //       source: `
+  //         mutation CreateNewOrderTestQuery($userId: String, $orderPayload: OrderGenerationInput!, $cardVersionPayload: CardVersionInput!) {
+  //           createNewOrder(userId: $userId, orderPayload: $orderPayload, cardVersionPayload: $cardVersionPayload) {
+  //             orderId
+  //             cardVersionId
+  //           }
+  //         }
+  //       `,
+  //       variableValues: {
+  //         userId: user.id,
+  //         orderPayload: orderPayload,
+  //         cardVersionPayload: cardVersionPayload,
+  //       },
+  //       asAdmin: true,
+  //     })
+
+  //     const newOrder = await Order.mongo.findById(response.data?.createNewOrder?.orderId)
+  //     const newCardVersion = await CardVersion.mongo.findById(
+  //       response.data?.createNewOrder?.cardVersionId
+  //     )
+
+  //     expect(newOrder.quantity).toEqual(orderPayload.quantity)
+  //     expect(newOrder.price).toEqual(orderPayload.price)
+  //     expect(newOrder.state).toEqual(OrderState.Captured)
+
+  //     const partialNewCardVersion = {
+  //       cardSlug: newCardVersion.cardSlug,
+  //       firstName: newCardVersion.name.first,
+  //       middleName: newCardVersion.name.middle,
+  //       lastName: newCardVersion.name.last,
+  //       phoneNumber: newCardVersion.phoneNumber,
+  //       email: newCardVersion.email,
+  //       title: newCardVersion.title,
+  //       company: newCardVersion.company,
+  //       addressLine1: newCardVersion.address.line1,
+  //       addressLine2: newCardVersion.address.line2,
+  //       addressLine3: newCardVersion.address.line3,
+  //       frontImageUrl: newCardVersion.frontImageUrl,
+  //       backImageUrl: newCardVersion.backImageUrl,
+  //     }
+  //     expect(partialNewCardVersion).toMatchObject(cardVersionPayload)
+
+  //     const orderUser = await User.mongo.findById(newOrder.user)
+  //     const cardVersionUser = await User.mongo.findById(newCardVersion.user)
+
+  //     expect(orderUser.id).toBe(user.id)
+  //     expect(cardVersionUser.id).toBe(user.id)
+  //   })
+
+  describe('OrderModel', () => {
+    it('detects a shortId collision and retries', async () => {
+      const order1 = await createMockOrder({ shortId: 'SJC123' })
+      const order2 = await createMockOrder({ shortId: 'SJC123' })
+      expect(order1.shortId).toBe('SJC123')
+      expect(order1.shortId).not.toEqual(order2.shortId)
     })
   })
 })
