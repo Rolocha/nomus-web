ef7f1d686912b97d436bf81c4b0f810691459a34
Anshul Aggarwal <anshulaggarw4l@gmail.com>
2021-02-03 22:26:58 -0800

    adding @authorized

--- server/src/graphql/resolvers/orderEvent.ts
+++ server/src/graphql/resolvers/orderEvent.ts
@@ -1,30 +1,28 @@
 import OrderEvent from 'src/models/OrderEvent'
 import { Role } from 'src/util/enums'
 import { IApolloContext } from 'src/graphql/types'
-import { Arg, Ctx, Query, Resolver } from 'type-graphql'
+import { Arg, Authorized, Ctx, Query, Resolver } from 'type-graphql'
 
 @Resolver()
 class OrderEventResolver {
   // Get a single OrderEvent
+  @Authorized(Role.Admin)
   @Query(() => OrderEvent, { nullable: true })
   async orderEvent(
     @Arg('orderEventId', { nullable: true }) orderEventId: string | null,
     @Ctx() context: IApolloContext
   ): Promise<OrderEvent> {
-    if (context.user.roles.includes(Role.Admin)) {
-      return await OrderEvent.mongo.findById(orderEventId).populate('order')
-    }
+    return await OrderEvent.mongo.findById(orderEventId).populate('order')
   }
 
   // Get all OrderEvents of an Order
+  @Authorized(Role.Admin)
   @Query(() => [OrderEvent], { nullable: true })
   async orderEventsForOrder(
     @Arg('orderId', { nullable: true }) orderId: string | null,
     @Ctx() context: IApolloContext
   ): Promise<OrderEvent[]> {
-    if (context.user.roles.includes(Role.Admin)) {
-      return await OrderEvent.mongo.find({ order: orderId }).sort({ createdAt: 1 })
-    }
+    return await OrderEvent.mongo.find({ order: orderId }).sort({ createdAt: 1 })
   }
 }
 export default OrderEventResolver
