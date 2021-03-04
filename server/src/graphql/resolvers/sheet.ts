3719a7e882be79267333ae60c8ba29cd8dbeb1d4
Anshul Aggarwal <OmegaNalphA@users.noreply.github.com>
2021-03-04 13:48:52 -0800

    adding field resolvers (#147)

--- server/src/graphql/resolvers/sheet.ts
+++ server/src/graphql/resolvers/sheet.ts
@@ -1,10 +1,19 @@
-import { Resolver, Query, Authorized, Arg } from 'type-graphql'
+import { Resolver, Query, Authorized, Arg, FieldResolver, Root } from 'type-graphql'
 
 import { Role } from 'src/util/enums'
-import { Sheet } from 'src/models'
+import { CardVersion, Order, Sheet } from 'src/models'
 
-@Resolver()
+@Resolver((of) => Sheet)
 class SheetResolver {
+  @FieldResolver()
+  async order(@Root() sheet: Sheet) {
+    return Order.mongo.findById(sheet.order)
+  }
+  @FieldResolver()
+  async cardVersion(@Root() sheet: Sheet) {
+    return CardVersion.mongo.findById(sheet.cardVersion)
+  }
+
   @Authorized(Role.Admin)
   @Query(() => Sheet, { nullable: true })
   async sheet(@Arg('sheetId', { nullable: false }) sheetId: string): Promise<Sheet> {
