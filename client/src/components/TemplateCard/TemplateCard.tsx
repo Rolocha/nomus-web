7fef2e6390cde01d8ef8c68993464d769c5d0abe
Bibek <ghim.bibek@gmail.com>
2021-04-06 22:00:52 -0700

    Add isRenderingRef to TemplateCard

--- client/src/components/TemplateCard/TemplateCard.tsx
+++ client/src/components/TemplateCard/TemplateCard.tsx
@@ -26,22 +26,27 @@ function TemplateCard<T extends TemplateID>({
   showGuides,
 }: Props<T>) {
   const canvasRef = React.useRef<HTMLCanvasElement | null>(null)
+  const isRenderingRef = React.useRef<boolean>(false)
 
   const updateCard = React.useCallback(
     async (canvas: HTMLCanvasElement) => {
       const template = templates[templateId]
 
+      if (isRenderingRef.current) return
+
+      isRenderingRef.current = true
       if (side === 'front') {
         await template.renderFront(canvas, options)
       } else if (side === 'back') {
         await template.renderBack(canvas, options)
       }
+      isRenderingRef.current = false
       template.drawBorder(canvas.getContext('2d')!)
       if (showGuides) {
         template.drawInnerBleed(canvas.getContext('2d')!)
       }
     },
-    [options, side, templateId, showGuides],
+    [options, side, templateId, showGuides, isRenderingRef],
   )
 
   React.useEffect(() => {
