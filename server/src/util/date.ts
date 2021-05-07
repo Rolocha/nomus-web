d2139d691d6c429920e619df40923a42961b7bff
Anshul Aggarwal <anshulaggarw4l@gmail.com>
2021-05-07 11:51:12 -0700

    fixing date logic

--- server/src/util/date.ts
+++ server/src/util/date.ts
@@ -1,8 +1,3 @@
-export const getCurrentDateForDateInput = () => {
-  return adjustDateByTZOffset(Date.now()).toISOString().substr(0, 10)
-}
-
-export const adjustDateByTZOffset = (date: string | Date | number) => {
-  const dateObj = new Date(date)
-  return new Date(+dateObj - dateObj.getTimezoneOffset() * 60000)
+export const getCurrentDateForDateInput = (): string => {
+  return new Date().toISOString()
 }
