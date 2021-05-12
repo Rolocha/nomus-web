93a851fc959af7ebe32e6c27218cccb0a5177233
Anshul Aggarwal <OmegaNalphA@users.noreply.github.com>
2021-05-12 14:10:26 -0700

    Update client/src/pages/ContactInfoPage.tsx
    Co-authored-by: Bibek Ghimire <ghim.bibek@gmail.com>

--- client/src/pages/ContactInfoPage.tsx
+++ client/src/pages/ContactInfoPage.tsx
@@ -217,7 +217,7 @@ const ContactInfoPage = () => {
             {(contact.cardFrontImageUrl || contact.cardBackImageUrl) && (
               <Box
                 gridArea="businessCard"
-                width={{ base: '25%', [bp]: '75%' }}
+                width={{ base: '25%', [bp]: '100%' }}
                 mb={{ base: 0, [bp]: 2 }}
                 mr={{ base: 2, [bp]: 0 }}
               >
