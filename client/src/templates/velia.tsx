0e09e7087ff9d16052c50ebee994e32824b1478b
Anshul Aggarwal <OmegaNalphA@users.noreply.github.com>
2021-05-26 09:39:27 -1000

    fixed velia bleed (#211)

--- client/src/templates/velia.tsx
+++ client/src/templates/velia.tsx
@@ -4,13 +4,13 @@ import CardTemplate, { CardTemplateRenderOptions } from 'src/templates/base'
 
 const Velia = new CardTemplate({
   name: 'Velia',
-  width: 252,
-  height: 144,
+  width: 264,
+  height: 154,
   demoImageUrl:
     'https://nomus-assets.s3.amazonaws.com/site-assets/templates/velia.svg',
   colorScheme: {
     background: {
-      defaultValue: '#ffffff',
+      defaultValue: colors.white,
     },
     accent: {
       defaultValue: colors.nomusBlue,
@@ -78,12 +78,12 @@ const Velia = new CardTemplate({
       options.colorScheme.accent ??
       this.colorSchemeSpec.accent.defaultValue ??
       // Should never have go this far
-      '#000000'
+      colors.nomusBlue
     ctx.fillRect(
       0,
-      this.proportionalize(128),
+      this.proportionalize(133),
       this.proportionalizedWidth,
-      this.proportionalize(16),
+      this.proportionalize(21),
     )
 
     // Render the name
@@ -96,7 +96,7 @@ const Velia = new CardTemplate({
       options.contactInfo.name ||
         this.contactInfoSpec.name.placeholder ||
         '[name]',
-      this.proportionalize(20 + 14),
+      this.proportionalize(25 + 14),
     )
 
     // Render the headline
@@ -109,7 +109,7 @@ const Velia = new CardTemplate({
       options.contactInfo.headline ||
         this.contactInfoSpec.headline.placeholder ||
         '[headline]',
-      this.proportionalize(40 + 8),
+      this.proportionalize(45 + 8),
     )
 
     // Render the 3 contact info lines, all 8px left from center
@@ -127,7 +127,7 @@ const Velia = new CardTemplate({
         ''
       const line1TextMetrics = ctx.measureText(line1Text)
       const line1TextX = rightEdgeForLines - line1TextMetrics.width
-      ctx.fillText(line1Text, line1TextX, this.proportionalize(65 + 7))
+      ctx.fillText(line1Text, line1TextX, this.proportionalize(70 + 7))
     }
 
     if (!options.omittedContactInfoFields.includes('line2')) {
@@ -140,7 +140,7 @@ const Velia = new CardTemplate({
         ''
       const line2TextMetrics = ctx.measureText(line2Text)
       const line2TextX = rightEdgeForLines - line2TextMetrics.width
-      ctx.fillText(line2Text, line2TextX, this.proportionalize(77 + 7))
+      ctx.fillText(line2Text, line2TextX, this.proportionalize(82 + 7))
     }
 
     if (!options.omittedContactInfoFields.includes('line3')) {
@@ -153,7 +153,7 @@ const Velia = new CardTemplate({
         ''
       const line3TextMetrics = ctx.measureText(line3Text)
       const line3TextX = rightEdgeForLines - line3TextMetrics.width
-      ctx.fillText(line3Text, line3TextX, this.proportionalize(89 + 7))
+      ctx.fillText(line3Text, line3TextX, this.proportionalize(94 + 7))
     }
 
     if (!options.omittedContactInfoFields.includes('footer')) {
@@ -169,14 +169,14 @@ const Velia = new CardTemplate({
       this.drawTextHorizontallyCenteredAtY(
         ctx,
         footerText,
-        this.proportionalize(108),
+        this.proportionalize(113),
       )
     }
 
     // Render QR code
     await this.drawQRCode(ctx, options.qrCodeUrl || 'https://nomus.me', {
-      x: this.proportionalize(134),
-      y: this.proportionalize(65),
+      x: this.proportionalize(140),
+      y: this.proportionalize(70),
       width: this.proportionalize(32),
       height: this.proportionalize(32),
       backgroundColor: options.colorScheme.background,
@@ -185,16 +185,16 @@ const Velia = new CardTemplate({
 
     // Render Nomus logo
     await this.drawNomusLogo(ctx, {
-      x: this.proportionalize(170),
-      y: this.proportionalize(70),
+      x: this.proportionalize(176),
+      y: this.proportionalize(75),
       size: this.proportionalize(11),
       color: options.colorScheme.accent,
     })
 
     // Render NFC tap icon
     await this.drawNFCTapIcon(ctx, {
-      x: this.proportionalize(170),
-      y: this.proportionalize(85),
+      x: this.proportionalize(176),
+      y: this.proportionalize(90),
       size: this.proportionalize(11),
       color: options.colorScheme.accent,
     })
@@ -211,14 +211,14 @@ const Velia = new CardTemplate({
       throw new Error('Got null for canvas context')
     }
 
-    const ACCENT_BAR_SIZE = 16
+    const ACCENT_BAR_SIZE = 21
 
     // Draw the main background with accent color
     ctx.fillStyle =
       options.colorScheme.accent ??
       this.colorSchemeSpec.accent.defaultValue ??
       // Should never have go this far
-      '#000000'
+      colors.nomusBlue
     ctx.fillRect(
       0,
       0,
@@ -231,12 +231,12 @@ const Velia = new CardTemplate({
       options.colorScheme.background ??
       this.contactInfoSpec.name.defaultValue ??
       // Should never have go this far
-      '#ffffff'
+      colors.white
     ctx.fillRect(
       0,
-      this.proportionalizedHeight - 100,
+      this.proportionalize(this.height - ACCENT_BAR_SIZE),
       this.proportionalizedWidth,
-      1000,
+      this.proportionalize(ACCENT_BAR_SIZE),
     )
 
     // Render user-provided logo if provided
@@ -247,10 +247,11 @@ const Velia = new CardTemplate({
       const imageWidth =
         (imageHeight * logoImg.naturalWidth) / logoImg.naturalHeight
 
-      // y + imageHeight + y + ACCENT_BAR_SIZE = this.usableHeight
-      // y = (this.usableHeight - ACCENT_BAR_SIZE - imageHeight) / 2
       const imageY =
-        (this.proportionalizedHeight - ACCENT_BAR_SIZE - imageHeight) / 2
+        (this.proportionalize(this.height) -
+          this.proportionalize(ACCENT_BAR_SIZE) -
+          imageHeight) /
+        2
       const imageX = (this.proportionalizedWidth - imageWidth) / 2
       ctx.drawImage(logoImg, imageX, imageY, imageWidth, imageHeight)
     }
