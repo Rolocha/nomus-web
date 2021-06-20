27b4cc38ea3d87013fe6a63714ddcf129b00279f
Bibek Ghimire <ghim.bibek@gmail.com>
2021-06-19 20:54:38 -0700

    Hide /graphql endpoint when on nomus.me

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,7 +1,8 @@
 import ms from 'ms'
 
 export const AUTH_TOKEN_PRIVATE_KEY = process.env.AUTH_TOKEN_PRIVATE_KEY
-export const NODE_ENV = process.env.NODE_ENV ?? 'development'
+export const DEPLOY_ENV = process.env.ENV as 'development' | 'staging' | 'production'
+export const NODE_ENV = (process.env.NODE_ENV ?? 'development') as 'development' | 'production'
 export const APP_SERVER_PORT = process.env.APP_SERVER_PORT ?? '3000'
 export const SENDGRID_TOKEN =
   process.env.NODE_ENV === 'test'
@@ -21,7 +22,7 @@ export const BASE_URL =
     development: 'http://localhost:1234',
     staging: 'https://stage.nomus.me',
     production: 'https://nomus.me',
-  }[process.env.ENV] ?? 'https://testing.nomus.me'
+  }[DEPLOY_ENV] ?? 'https://testing.nomus.me'
 
 export const ACCESS_TOKEN_COOKIE_NAME = 'X-Access-Token'
 export const REFRESH_TOKEN_COOKIE_NAME = 'X-Refresh-Token'
--- server/src/graphql/index.ts
+++ server/src/graphql/index.ts
@@ -1,4 +1,5 @@
 import { ApolloServer } from 'apollo-server-express'
+import { DEPLOY_ENV } from 'src/config'
 
 import schema from 'src/graphql/schema'
 
@@ -6,12 +7,15 @@ export const server = new ApolloServer({
   schema,
   // When we deploy to prod, we may want to disable this but in a way where we can still get
   // the playground in staging
-  playground: {
-    settings: {
-      'request.credentials': 'same-origin',
-    },
-  },
-  introspection: true,
+  playground:
+    DEPLOY_ENV !== 'production'
+      ? {
+          settings: {
+            'request.credentials': 'same-origin',
+          },
+        }
+      : false,
+  introspection: DEPLOY_ENV !== 'production',
   context: ({ req }) => {
     if (req.user) {
       return { user: req.user }
