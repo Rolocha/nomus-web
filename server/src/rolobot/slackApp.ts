f5133179228943640b7cb845fb1a3cf98718d331
Anshul Aggarwal <anshulaggarw4l@gmail.com>
2021-06-24 23:51:02 -0700

    cleanup

--- server/src/rolobot/slackApp.ts
+++
@@ -1,9 +0,0 @@
-import { App } from '@slack/bolt'
-import { SLACK_APP_TOKEN, SLACK_BOT_TOKEN, SLACK_SIGNING_SECRET } from 'src/config'
-
-export default new App({
-  token: SLACK_BOT_TOKEN,
-  signingSecret: SLACK_SIGNING_SECRET,
-  socketMode: true,
-  appToken: SLACK_APP_TOKEN,
-})
