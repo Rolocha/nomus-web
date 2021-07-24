fddb5a9f2d82363c40de462d2650712460c3ee14
heatman7 <66530402+heatman7@users.noreply.github.com>
2021-07-24 01:49:28 -0700

    add position sticky to wizard in desktop view

--- client/src/components/Wizard/Wizard.tsx
+++ client/src/components/Wizard/Wizard.tsx
@@ -146,7 +146,7 @@ function Wizard<ValidStepType extends string>({
         // Needed to ensure the current-tab caret indicator is visible
         overflow="visible"
         // Need these 2 to make pointy indicator show up
-        position="relative"
+        position={{ base: 'relative', [bp]: 'sticky' }}
         zIndex={20}
       >
         {allStepDetails.map((stepDetails, index) => {
