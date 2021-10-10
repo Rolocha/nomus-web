0f9c42cec731f06e4b7badc3c4e4159e2da10f9a
Bibek Ghimire <ghim.bibek@gmail.com>
2021-10-09 22:40:24 -0700

    ts-ignore on DeletedObjec modelOptions arg._id

--- server/src/models/DeletedObject.ts
+++ server/src/models/DeletedObject.ts
@@ -1,7 +1,14 @@
 import { getModelForClass, modelOptions, prop, ReturnModelType } from '@typegoose/typegoose'
 
 // We can't use/extend BaseModel in here because BaseModel imports this class and that results in a circular dependency
-@modelOptions({ schemaOptions: { timestamps: true, usePushEach: true, _id: String } })
+@modelOptions({
+  schemaOptions: {
+    // @ts-ignore Bad types from typegoose here falsely require a boolean
+    _id: String,
+    timestamps: true,
+    usePushEach: true,
+  },
+})
 class DeletedObject {
   static mongo: ReturnModelType<typeof DeletedObject>
 
